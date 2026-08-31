162edda4e8e5caf79262727c2ce7357323081419
Eco9341 <mastronardiluca7@gmail.com>
2026-09-01 00:45:56 +0200

    first commit

---
+++ RussianSFPack/Helpers/EcoQuestHelper.cs
@@ -0,0 +1,137 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Services;
+using WTTServerCommonLib.Helpers;
+
+namespace RussianSFPack.Helpers
+{
+    [Injectable]
+    public class EcoQuestHelper(DatabaseService  databaseService, ISptLogger<EcoQuestHelper> logger, QuestHelper questHelper)
+    {
+
+        // Define weapon IDs
+        // ReSharper disable InconsistentNaming
+        // ReSharper disable IdentifierTypo
+        //snipers
+
+        //shotguns
+        
+        //smg
+
+        //pistols
+        
+        //rifles 
+        private const string AM17 = "6a9417f9e097b0c6ce414793";
+        private const string AMB17 = "6a8ada430d43213c92dbe371";
+        
+        //dmr
+        
+        // Weapon Mods
+        
+        public void ModifyQuests()
+        {
+            var quests = databaseService.GetTemplates().Quests;
+
+            // ReSharper disable CommentTypo
+            // ====================== PRAPOR QUESTS ======================
+            //
+            // // Punisher Part 4 (59ca264786f77445a80ed044)
+            // questHelper.AddWeaponsToKillCondition(quests, "59ca264786f77445a80ed044", [
+            //     BP12, BP12_GEN2, B1301, B1301_FDE, B1301_GREEN
+            // ]);
+
+            // // Mall Cop (64e7b99017ab941a6f7bf9d7)
+            // questHelper.AddWeaponsToKillCondition(quests, "64e7b99017ab941a6f7bf9d7", [
+            //     MK22_HUSHPUPPY, AMT_HARDBALLER
+            // ]);
+
+            // Tickets, Please (64e7b9a4aac4cd0a726562cb)
+             /*questHelper.AddWeaponsToKillCondition(quests, "64e7b9a4aac4cd0a726562cb", [
+
+             ]);*/
+
+            // District Patrol (64e7b9bffd30422ed03dad38)
+            questHelper.AddWeaponsToKillCondition(quests, "64e7b9bffd30422ed03dad38", [
+                AM17, AMB17
+            ]);
+
+            // ====================== SKIER QUESTS ======================
+
+            // // Stirrup (596b455186f77457cb50eccb)
+            // questHelper.AddWeaponsToKillCondition(quests, "596b455186f77457cb50eccb", [
+            //     MK22_HUSHPUPPY, AMT_HARDBALLER
+            // ]);
+
+            // // Silent Caliber (5c0bc91486f7746ab41857a2)
+            // questHelper.AddWeaponsToKillCondition(quests, "5c0bc91486f7746ab41857a2", [
+            //     BP12, BP12_GEN2, B1301, B1301_FDE, B1301_GREEN
+            // ]);
+            //
+            // // Setup (5c1234c286f77406fa13baeb)
+            // questHelper.AddWeaponsToKillCondition(quests, "5c1234c286f77406fa13baeb", [
+            //     BP12, BP12_GEN2, B1301, B1301_FDE, B1301_GREEN
+            // ]);
+
+            // Connections Up North (6764174c86addd02bc033d68)
+            /*questHelper.AddWeaponsToKillCondition(quests, "6764174c86addd02bc033d68", [
+
+            ]);*/
+
+            // ====================== PEACEKEEPER QUESTS ======================
+
+            // // Spa Tour Part 1 (5a03153686f77442d90e2171)
+            // questHelper.AddWeaponsToKillCondition(quests, "5a03153686f77442d90e2171", [
+            //     BP12, BP12_GEN2, B1301, B1301_FDE, B1301_GREEN
+            // ]);
+            //
+            // // Worst Job (63a9b229813bba58a50c9ee5)
+            // questHelper.AddWeaponsToKillCondition(quests, "63a9b229813bba58a50c9ee5", [
+            //     LR300, LR300_FDE, TT_TR1, BRN180, BRN180_FDE
+            // ]);
+
+            // ====================== JAEGER QUESTS ======================
+
+            /*var tarkovShooterWeapons = new[]
+            {
+
+            };
+
+            // Tarkov Shooter Part 1-8 (WEAPONS)
+            questHelper.AddWeaponsToKillCondition(quests, "5bc4776586f774512d07cf05", tarkovShooterWeapons); // Part 1
+            questHelper.AddWeaponsToKillCondition(quests, "5bc479e586f7747f376c7da3", tarkovShooterWeapons); // Part 2
+            questHelper.AddWeaponsToKillCondition(quests, "5bc47dbf86f7741ee74e93b9", tarkovShooterWeapons); // Part 3
+            questHelper.AddWeaponsToKillCondition(quests, "5bc480a686f7741af0342e29", tarkovShooterWeapons); // Part 4
+            questHelper.AddWeaponsToKillCondition(quests, "5bc4826c86f774106d22d88b", tarkovShooterWeapons); // Part 5
+            questHelper.AddWeaponsToKillCondition(quests, "5bc4836986f7740c0152911c", tarkovShooterWeapons); // Part 6
+            questHelper.AddWeaponsToKillCondition(quests, "5bc4856986f77454c317bea7", tarkovShooterWeapons); // Part 7
+            questHelper.AddWeaponsToKillCondition(quests, "5bc4893c86f774626f5ebf3e", tarkovShooterWeapons); // Part 8
+
+            // Tarkov Shooter Part 1-8 (MODS)
+            
+            // Part 1
+            
+            // Part 7
+
+
+            
+            // // Claustrophobia (669fa3979b0ce3feae01a130)
+            // questHelper.AddWeaponsToKillCondition(quests, "669fa3979b0ce3feae01a130", [
+            //     BP12, BP12_GEN2, B1301, B1301_FDE, B1301_GREEN
+            // ]);
+
+            // ====================== MECHANIC QUESTS ======================
+
+            // Psycho Sniper (5c0be13186f7746f016734aa)
+            questHelper.AddWeaponsToKillCondition(quests, "5c0be13186f7746f016734aa", [
+
+            ]);
+
+            // Shooter Born in Heaven (5c0bde0986f77479cf22c2f8)
+            questHelper.AddWeaponsToKillCondition(quests, "5c0bde0986f77479cf22c2f8", [
+
+            ]);*/
+        }
+    }
+}
---
+++ RussianSFPack/RussianSFPack.cs
@@ -0,0 +1,47 @@
+﻿using RussianSFPack.Helpers;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Services;
+using System.Reflection;
+using WTTServerCommonLib.Helpers;
+using WTTServerCommonLib.Models;
+using WTTServerCommonLib.Services;
+using Range = SemanticVersioning.Range;
+
+namespace RussianSFPack;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.wtt.russiansfpack";
+    public override string Name { get; init; } = "Russian-SF-Pack";
+    public override string Author { get; init; } = "Eco";
+    public override List<string>? Contributors { get; init; } = null;
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public override Range SptVersion { get; init; } = new("~4.0.13");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.24") }
+    };
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "MIT";
+}
+
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class RussianSFPack(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon,
+    EcoQuestHelper ecoQuestHelper) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
+        ecoQuestHelper.ModifyQuests();
+        await Task.CompletedTask;
+    }
+}
